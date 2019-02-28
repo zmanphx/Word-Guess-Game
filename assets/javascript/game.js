86804a1cc93f0c6a8c47a57edc03fa156ce9d414
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-02-28 12:51:00 -0700

    another partial commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,204 +2,163 @@
 
 
 
-  //  var scr_drivetowork = document.getElementById("drivetowork");
-  //  var Scr_drivearound = document.getElementById("drivearound");
- //   var Scr_getTuneUp = document.getElementById("getTuneUp");
- //   var Scr_honk = document.getElementById("honk");
+//  var scr_drivetowork = document.getElementById("drivetowork");
+//  var Scr_drivearound = document.getElementById("drivearound");
+//   var Scr_getTuneUp = document.getElementById("getTuneUp");
+//   var Scr_honk = document.getElementById("honk");
 //    var Scr_start = document.getElementById("Start");
 
 
-  
-   
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function (event) {
-      Scr_start.textContent = "";
-      var userGuess = event.key;
-      
+
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+  Scr_start.textContent = "";
+  var userGuess = event.key;
+
+}
+
+var guessArr = [];
+
+
+function reply_click() {
+  whichtheme(event.srcElement.id)
+
+}
+
+
+
+function whichtheme(btnid) {
+  var diner = ["McDonalds", "Pizza Hut", "Taco Bell", "Olive Garden", "Panda Express"];
+  var eightyspop = ["Elton John", "Michael Jackson", "Phil Collins", "Prince", "Billy Joel"];
+  var actionHero = ["Super Man", "Spider Man", "Wonder Woman", "Batman", "Captain America"];
+
+  wordToguess = "";
+
+  if (btnid === "btnFood") {
+    guessArr = shuffle(diner);
+
+  }
+  else
+    if (btnid === "btn80pop") {
+
+      guessArr = shuffleArray(eightyspop);
+
     }
-      
-    var guessArr =[];
-    var guessWord ="";  
+    else {
+      guessArr = shuffleArray(actionHero);
+
+    }
+}
+
+function shuffle(array) {
+  var i = 0
+    , j = 0
+    , temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array;
+}
 
 
-    function reply_click()
-      {
-        var diner = ["McDonalds","Pizza Hut", "Taco Bell", "Olive Garden", "Panda Express" ];
-        var eightyspop = ["Elton John", "Michael Jackson", "Phil Collins", "Prince", "Billy Joel" ];
-        var actionHero  = ["Super Man", "Spider Man", "Wonder Woman", "Batman", "Captain America"];
-        
-        wordToguess="";
-       
-       
-           console.log(event.srcElement.id);
-       
-        if(event.srcElement.id === "btnFood") {
-          guessArr= shuffle(diner);
-                  
-        }
-         else
-        if(event.srcElement.id === "btn80pop") {
-        
-          guessArr= shuffleArray( eightyspop );
 
-          }  
-          else
-          {
-             guessArr= shuffleArray(actionHero );
+//Check which button was pressed.
+// Change background screen image
+// load array to the game object
+// create loop with prompt count up to 5 max "Do you want to play again ?"
+// if user wants to play the same theme 
 
-           }
-        
+// pass array to game object
+
+// jquery to capture which key was pressed/
+//$(function(){
+//  $(document).on('keypress', function(e){
+//      console.log(e.which);
+ // });
+//  });
+//
+
+var guessWordSoFar = "";
+
+// Creating our car object.
+
+for (i = 0; i < 5; i++) {   // Master outerloop
+
+  wordToguess = guessArr[i];
+  
+    //firstcreate the blank word with empty spaces to account for the length
+  if( i===0){ 
+              guessWordSoFar = game.makeblank();} 
+            
+
+
+
+
+
+/*************************************** * game object below*********************************************************************/
+  var game = {
+
+    //This function creates a blank unpopulated string to account for each letter position and space. 
+    //This should be called when starting a new word search /
+    makeblank: function () {
+      var blankstr;
+      for (i = 0; i < wordToguess.length - 1; i++) {
+        if (wordToguess[i] !== " ") { blankstr = blankstr & "#" }
+        else { blankstr = blankstr & " " }
+
       }
-      
-        function whichtheme( )
-        {
-          var diner = ["McDonalds","Pizza Hut", "Taco Bell", "Olive Garden", "Panda Express" ];
-          var eightyspop = ["Elton John", "Michael Jackson", "Phil Collins", "Prince", "Billy Joel" ];
-          var actionHero  = ["Super Man", "Spider Man", "Wonder Woman", "Batman", "Captain America"];
-          
-          wordToguess="";
-         
-         
-             console.log(event.srcElement.id);
-         
-          if(event.srcElement.id === "btnFood") {
-            guessArr= shuffle(diner);
-                    
-          }
-           else
-          if(event.srcElement.id === "btn80pop") {
-          
-            guessArr= shuffleArray( eightyspop );
-  
-            }  
-            else
-            {
-               guessArr= shuffleArray(actionHero );
-  
-             }
-          
-        }
-        
-
-
-        }
-
-
-
-
-      function shuffle (array) {
-       
-       
-        var i = 0
-          , j = 0
-          , temp = null
-      
-        for (i = array.length - 1; i > 0; i -= 1) {
-          j = Math.floor(Math.random() * (i + 1))
-          temp = array[i]
-          array[i] = array[j]
-          array[j] = temp
-        }
-        return array;
-      }
+      return blankstr
+    },
   
 
+    // This function "checkguess" checks the user input letter against the word and returns populated string with succesful guesses
+    // or returns not found. 
+    checkguess: function (fullword, blankstr, checkLetter) 
+    {     var blankstr;
+          var indices=[];
+          for(var i=0; i<str.length;i++) {
+            if (fullword[i]===checkLetter)  blankstr = replaceA(blankstr, i, checkLetter); //indices.push[i];
 
-      //Check which button was pressed.
-    // Change background screen image
-    // load array to the game object
-    // create loop with prompt count up to 5 max "Do you want to play again ?"
-    // if user wants to play the same theme 
-    
-     // pass array to game object
-      
-         wordToguess= guessArr[0]; 
+      }        
+      return blankstr
+    },
 
+  // replace charater at postion. 
+     replaceAt: function(string, index, replace)
+     {
+      return string.substring(0, index) + replace + string.substring(index + 1);
+    }
+,
+    max_guesses: 10,
+    guess_left: 10,
+    lengthofitem: 0,
+    guessletter:""
 
-      // Creating our car object.
-      var game = {
+  }
+  /**************************************************game object above************************************** */
+ 
+  guessWordSoFar = game.makeblank();  // This is the blank word with spaces
+  
+  // game object has to populate the screen based on the word passed to it. 
+  // get the length 
 
-        // Properties of our car object.
-        // game object has to populate the screen based on the word passed to it. 
-        // get the length 
-
-        // set up loop to  listen to key events
-        //  User enter a letter,  create function to search word for the letter
-          // if successful  populate the  line position with the  letter .  if not successful 
-          // prompt the user that the guess was wrong and delete from the total.
-           // if guess exceeds 10 . reveal the word.  Prompt user if they want to play again.  
-
-        max_guesses:10,
-        guess_left:10,
-        lengthofitem: 0,
-        currentItem:"A",
-        
-        
-         // currentItem: guessArr[0],
-         // lengthofitem: currentItem.length,
-        
-        
-        // Creating the driveToWork method.
-        // A method is a function associated with an object.
-        driveToWork: function () {
-
-          // When we say "this" (as in this.mileage) we are referring to the object the method is a part of.
-          // In this case, we will alert the mileage of the car object.
-          alert("Old Mileage: " + this.mileage);
-
-          // Adding 8 to the car's mileage.
-          this.mileage = this.mileage + 8;
-
-          // Alerting the car's new mileage.
-          alert("New mileage: " + this.mileage);
-        },
-
-        // The driveAroundWorld method adds 24,000 miles to our car, sets isWorking to false, and makes some alerts.
-        driveAroundWorld: function () {
-
-          alert("Old Mileage: " + this.mileage);
-
-          this.mileage = this.mileage + 24000;
-
-          alert("New Mileage: " + this.mileage);
-          alert("Car needs a tuneup!");
-
-          this.isWorking = false;
-        },
-
-        // The getTuneUp method sets isWorking to true and alerts a message.
-        getTuneUp: function () {
-          alert("Car is ready to go!");
-          this.isWorking = true;
-        },
-
-        // The honk method alerts a honking message.
-        honk: function () {
-          alert("Honk! Honk!");
-        }
-      };
+  // set up loop to  listen to key events
+  //  User enter a letter,  create function to search word for the letter
+  // if successful  populate the  line position with the  letter .  if not successful 
+  // prompt the user that the guess was wrong and delete from the total.
+  // if guess exceeds 10 . reveal the word.  Prompt user if they want to play again.  
 
 
 
 
+}
 
-      // How would we log...
 
-      // The car's make?
-    //  console.log(car.make);
 
-      // The car's model?
-    //  console.log(car.model);
 
-      // The car's mileage?
-     // console.log(car.mileage);
-
-      // How would we run the car's driveToWork method?
-    //  car.driveToWork();
-
-      // How would we run the car's driveAroundWorld method?
-    //  car.driveAroundWorld();
-
-      // How would we run the getTuneUp method?
-    //  car.getTuneUp();
 
